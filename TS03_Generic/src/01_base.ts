0028196aaac47d5e45a35ff64b6dd732bd3ff54f
TolkinSL <tolkinsl@yandex.ru>
2025-10-21 00:39:13 +0300

    TS03 Generic_1

--- TS03_Generic/src/01_base.ts
+++ TS03_Generic/src/01_base.ts
@@ -9,3 +9,15 @@ toArray(1, 2, 3);
 toArray('ab', 'cd');
 
 console.log(toArray('abc', 'cde'));
+
+interface Model<T> {
+  title: string;
+  value: T;
+}
+
+const myModel: Model<number> = {
+  title: 'Tesla',
+  value: 2009,
+}
+
+myModel.value;
